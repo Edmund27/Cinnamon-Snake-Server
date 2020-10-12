1de3df29613a41df2aeadfe12b5c10b13e7ce3ab
edmund27 <epafko@gmail.com>
2020-10-12 16:56:14 +0200

    Made connection with client through socket.io

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express')
 const PORT = process.env.PORT || 3000
 const cors = require('cors')
 const app = express()
+const socket = require('socket.io')
 
 app.use(cors());
 
 const server = app.listen(
     PORT, () => console.log(`Server running on port ${PORT}`)
 )
+
+const io = socket(server);
+
+io.on('connection', socket => {
+    console.log('new connection')
+    socket.emit('connected', "hello world!")
+})
