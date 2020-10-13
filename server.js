433c686620d5b2e5278a80dd09611e8e09a2056f
edmund27 <epafko@gmail.com>
2020-10-13 15:52:42 +0200

    Created loop that moves snake and ability to change directions

--- server.js
+++ server.js
@@ -21,15 +21,91 @@ let snake = [
     { x: 120, y: 150 },
     { x: 110, y: 150 },
 ];
+let dx = 10
+let dy = 0
+let speed = 100
+let changingDirection = false
 
 //.
 
 io.on('connection', socket => {
     console.log("New Connection")
-    const gameStateObject = {
-        snake: snake,
-        canvas: canvas
+
+    socket.on("startGame", () => {
+        snake = [
+            { x: 150, y: 150 },
+            { x: 140, y: 150 },
+            { x: 130, y: 150 },
+            { x: 120, y: 150 },
+            { x: 110, y: 150 },
+        ];
+
+        dx = 10
+        dy = 0
+
+        score = 0;
+        changingDirection = false
+
+        main()
+    })
+
+    socket.on("changeDirection", (keyPressed) => {
+        changeDirection(keyPressed)
+    })
+
+    function main() {
+        setTimeout(function onTick() {
+            changingDirection = false
+            moveSnake()
+            const gameStateObject = {
+                snake: snake,
+            }
+
+            socket.emit('gameState', gameStateObject)
+            main()
+        }, speed)
     }
 
-    socket.emit('gameState', gameStateObject)
+    function moveSnake() {
+        const head = { x: snake[0].x + dx, y: snake[0].y + dy }
+        snake.unshift(head)
+        snake.pop();
+    }
+
+    function changeDirection(keyPressed) {
+
+        const LEFT_KEY = 37;
+        const RIGHT_KEY = 39;
+        const UP_KEY = 38;
+        const DOWN_KEY = 40;
+
+        if (changingDirection) return
+
+        changingDirection - true
+
+        const goingUp = dy === -10;
+        const goingDown = dy === 10;
+        const goingRight = dx === 10;
+        const goingLeft = dx === -10;
+
+        if (keyPressed === LEFT_KEY && !goingRight) {
+            dx = -10
+            dy = 0
+        }
+
+        if (keyPressed === UP_KEY && !goingDown) {
+            dx = 0
+            dy = -10
+        }
+
+        if (keyPressed === RIGHT_KEY && !goingLeft) {
+            dx = 10
+            dy = 0
+        }
+
+        if (keyPressed === DOWN_KEY && !goingUp) {
+            dx = 0
+            dy = 10
+        }
+    }
 })
